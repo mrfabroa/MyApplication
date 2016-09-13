a1ac471668841678a674dd5750af8819aab1b16b
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2016-09-13 18:23:38 -0400

    pull test

--- app/src/main/java/com/example/eric/myapplication/MainActivity.java
+++ app/src/main/java/com/example/eric/myapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.util.Log;
 import android.widget.Toast;
 
-// COMMIT THIS!!!!!
+// COMMIT THIS!!!!!  New Update
 
 public class MainActivity extends AppCompatActivity {
 
