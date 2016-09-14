c660fc7f562d385e6adb235181d5d44ac92a4bea
mrfabroa <eric.fabroa@ycdsbk12.ca>
2016-09-13 21:30:06 -0400

    test commit

--- app/src/main/java/com/example/eric/myapplication/MainActivity.java
+++ app/src/main/java/com/example/eric/myapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.util.Log;
 import android.widget.Toast;
 
-// COMMIT THIS!!!!!  New Update 2
+// COMMIT THIS!!!!!  New Update 3
 
 public class MainActivity extends AppCompatActivity {
 
